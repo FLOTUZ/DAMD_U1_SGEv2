c2dba7419efec9ea3ec18f1d208103ef543ae1a5
Emmanuel <flotuz10@gmail.com>
2021-04-08 13:38:50 -0500

    Se creó el diseño del Login

---
+++ app/src/main/java/com/example/damd_u1_sgev2/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.example.damd_u1_sgev2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.damd_u1_sgev2.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/example/damd_u1_sgev2/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.damd_u1_sgev2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.damd_u1_sgev2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    //Se declara el binding
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
